1292fcc356358372c69a482e7502e1451966c0d3
Vitaliy Zarubin <keygenqt@pinxterapp.com>
2020-03-06 23:35:16 +0300

    fix lib yii2-fontawesome

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -2,21 +2,14 @@
 
 namespace keygenqt\jsonForm;
 
-use \yii\web\AssetBundle;
+use yii\web\AssetBundle;
 
 /**
  * @author KeyGen <keygenqt@gmail.com>
  */
-class FontAwesomeAsset extends AssetBundle 
+class FontAwesomeAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/font-awesome'; 
-    public $css = [ 
-        'css/font-awesome.min.css', 
-    ];
-    public $publishOptions = [
-        'only' => [
-            'fonts/*',
-            'css/font-awesome.min.css',
-        ]
+    public $depends = [
+        'yidas\yii\fontawesome\FontawesomeAsset'
     ];
 }  
